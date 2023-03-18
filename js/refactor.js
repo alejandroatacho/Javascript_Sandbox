d2abc6791f0e06c628b7961aa6f74d642e7e8fd7
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-18 19:25:29 -0400

    s

---
+++ js/refactor.js
@@ -0,0 +1,22 @@
+// Sure, here's a simple project idea that you can try to implement in JavaScript:
+
+// Create a program that calculates the factorial of a given number.
+
+// To calculate the factorial of a number, you multiply that number by every positive integer less than it. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+
+// You can use a for loop to iterate through the positive integers less than the given number and keep a running total of the product. Then, you can print the result to the console.
+
+// This project should take less than 10 minutes to complete and will give you a good introduction to loops and basic math operations in JavaScript.
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+
+// rl.question('Write a number down: ', (answer) => {
+//   console.log(`The number you wrote is: ${answer}`);
+//   rl.close();
+// });
