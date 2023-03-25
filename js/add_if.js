e8a17ddc21912b229124707f33bc5766aa13eea9
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-25 19:14:32 -0400

    upload

---
+++ js/add_if.js
@@ -0,0 +1,8 @@
+let add = (num1, num2) => num1 + num2;
+
+let result = add(15, 5);
+if (result >= 5) {
+    console.log("Your answer is: " + result);
+} else {
+    console.log("error")
+}
---
+++ js/constructor_function.js
@@ -0,0 +1,1 @@
+let alien1 = alien('hina','JS')
