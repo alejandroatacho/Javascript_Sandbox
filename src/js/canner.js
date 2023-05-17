39368e22bcd460c01fa12f18287b773667a6f4df
Hinamizawa <hinamizawa.github@gmail.com>
2023-05-17 09:47:34 -0400

    update

---
+++ src/js/canner.js
@@ -0,0 +1,9 @@
+const default_value = 15;
+var num_2 = prompt(input("Enter value: "));
+var decision_tree = true;
+
+if (decision_true != false)
+{
+    var decision_tree = false;
+    console.log("Decision tree is true" + " " + num_2);
+}
